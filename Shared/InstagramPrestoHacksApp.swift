166165d2cc9d04c5a66ff377b70d6aa805e22dcc
Keith Lee <keith@zumper.com>
2021-12-11 11:10:19 -0800

    Initial Commit

---
+++ Shared/InstagramPrestoHacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramPrestoHacksApp.swift
+//  Shared
+//
+//  Created by Keith Lee on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramPrestoHacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
